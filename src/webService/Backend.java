96b62b5d6690f66164b06400293172f3747dd28a
Martin Leiacker <martin.leiacker@logos-logistics.de>
2020-03-21 00:12:51 +0100

    with POST

--- src/webService/Backend.java
+++ src/webService/Backend.java
@@ -1,12 +1,20 @@
 package webService;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 
 @Path ("/Backend")
 public class Backend {
 
+	@POST
+	@Path ("/product")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
 	public ProductResponse product(@Context HttpServletRequest request, ProductRequest req) {
 		ProductResponse response = null;
 		
