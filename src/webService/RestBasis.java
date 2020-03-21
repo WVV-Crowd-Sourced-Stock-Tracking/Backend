2d3b7a251be25214779145401b03e2a3341e0571
Martin Leiacker <martin.leiacker@logos-logistics.de>
2020-03-22 00:54:47 +0100

    private data source

--- src/webService/RestBasis.java
+++ src/webService/RestBasis.java
@@ -2,6 +2,7 @@ package webService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -9,18 +10,38 @@ import javax.naming.NamingException;
 import javax.servlet.http.HttpServlet;
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+
 public class RestBasis extends HttpServlet{
 	private static final long serialVersionUID = 1L;
-	private final static String DBRESOURCE = "jdbc/wvv";
-	private static Context initCtx = null;
-	private static Context envCtx = null;
-	private static DataSource dataSource = null;
+//	private final static String DBRESOURCE = "jdbc/wvv";
+//	private static Context initCtx = null;
+//	private static Context envCtx = null;
+//	private static DataSource dataSource = null;
+	
+	private static BasicDataSource dataSource = null;
+
 	protected Connection con = null;
 
-	protected Connection initWS() throws NamingException, SQLException {
+	protected Connection initWS() throws Exception {
 		Connection con = null;
 		boolean ret = true;
 		if (dataSource == null) {
+			
+			String d = "org.postgresql.Driver";
+			String u = "jdbc:postgresql://ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/d1ssropt4bpql2";
+			Properties dbcpProperties = new Properties();
+			dbcpProperties.put("driverClassName", d);
+			dbcpProperties.put("url", u);
+			dbcpProperties.put("username", "jqjbfxsjohmmhe" );
+			dbcpProperties.put("password", "bc7cf56183d22703c7915ffedd717fae431cd6847152ad572035fec60fff33d4" );
+			dbcpProperties.put("defaultAutoCommit", "FALSE");
+			dbcpProperties.put("maxActive", "5");
+			dbcpProperties.put("ssl", "true");
+
+			dataSource = (BasicDataSource) BasicDataSourceFactory.createDataSource(dbcpProperties);
+/*			
 			if (envCtx == null) {
 				if (initCtx == null) {
 					initCtx = new InitialContext();
@@ -28,6 +49,7 @@ public class RestBasis extends HttpServlet{
 				envCtx = (Context) initCtx.lookup("java:comp/env");
 			}
 			dataSource = (DataSource) envCtx.lookup(DBRESOURCE);
+*/
 			if (dataSource == null) {
 				ret = false;
 			}
