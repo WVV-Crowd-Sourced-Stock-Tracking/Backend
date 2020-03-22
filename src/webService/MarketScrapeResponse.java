66ed5bcbcf62e5c8d8a8fcd72a075d884790fd11
Dirk Leiacker <dirk.leiacker@stud.tu-darmstadt.de>
2020-03-22 13:41:22 +0100

    Markets zu Response hinzugefügt /market/scrape

--- src/webService/MarketScrapeResponse.java
+++ src/webService/MarketScrapeResponse.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import tools.GenericResponse;
+import tools.json_items.ProductItem;
 import tools.json_items.SupermarketItem;
 
 public class MarketScrapeResponse extends GenericResponse {
 	private List<SupermarketItem> supermarket = new ArrayList<SupermarketItem>();
+	private List<ProductItem> productItems = new ArrayList<ProductItem>();
 
 	public List<SupermarketItem> getSupermarket() {
 		return supermarket;
@@ -15,5 +17,13 @@ public class MarketScrapeResponse extends GenericResponse {
 
 	public void setSupermarket(List<SupermarketItem> supermarket) {
 		this.supermarket = supermarket;
+	}
+
+	public List<ProductItem> getProductItems() {
+		return productItems;
+	}
+
+	public void setProductItems(List<ProductItem> productItems) {
+		this.productItems = productItems;
 	}
 }
